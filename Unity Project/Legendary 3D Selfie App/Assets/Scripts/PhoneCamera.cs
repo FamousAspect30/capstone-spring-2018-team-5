b532e92cf795a35464d29c2896e1f5ccb3d95c7f
Wooton <jmwooton@cofc.edu>
2018-02-19 19:03:26 -0500

    Added a button to switch between rear and front-facing cameras (although it is broken as hell)

---
+++ Unity Project/Legendary 3D Selfie App/Assets/Scripts/PhoneCamera.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// credit to N3K EN at https://www.youtube.com/watch?v=c6NXkZWXHnc&t=55s for the tutorial used to write this script for accessing the camera(s) on devices
+
+public class PhoneCamera : MonoBehaviour {
+
+    private bool camAvailable;
+    private WebCamTexture selectedCamera;
+    private WebCamTexture frontCam;
+    private WebCamTexture backCam;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    private void Start()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if(devices.Length == 0)
+        {
+            Debug.Log("No camera detected");
+            camAvailable = false;
+            return;
+        }
+
+        for(int i=0; i< devices.Length; i++)
+        {
+            if (!devices[i].isFrontFacing)
+            {
+                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+
+            if (devices[i].isFrontFacing)
+            {
+                frontCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+        if (backCam == null)
+        {
+            Debug.Log("Unable to detect rear-facing camera.");
+            return;
+        }
+
+        if (frontCam == null)
+        {
+            Debug.Log("Unable to detect front-facing camera.");
+            return;
+        }
+
+        selectedCamera = backCam;
+        backCam.Play();
+        background.texture = backCam;
+
+        camAvailable = true;
+
+    }
+
+    private void Update()
+    {
+        if (!camAvailable)
+            return;
+
+        float ratio = (float)frontCam.width / (float)frontCam.height;
+        fit.aspectRatio = ratio;
+
+        float scaleY = frontCam.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -frontCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+
+    public void SwitchCamera()
+    {
+        if (selectedCamera == backCam)
+        {
+            selectedCamera = frontCam;
+            frontCam.Play();
+            background.texture = frontCam;
+        }
+        else
+        {
+            selectedCamera = backCam;
+            backCam.Play();
+            background.texture = backCam;
+        }
+    }
+}
