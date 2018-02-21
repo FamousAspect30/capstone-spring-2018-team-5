a54d228624cb310ddb6098a4aec0a84aab24cc91
Baylee Sims <simsbk@g.cofc.edu>
2018-02-21 13:21:39 -0500

    Capture Button moves to next screen

---
+++ Unity Project/Legendary 3D Selfie App/Assets/Scripts/CapturePic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
+
+public class CapturePic : MonoBehaviour {
+
+	public void Capture () {
+		SceneManager.LoadScene("Save Screen");
+	}
+
+}
--- Unity Project/Legendary 3D Selfie App/Assets/Scripts/MainMenu.cs
+++ Unity Project/Legendary 3D Selfie App/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
 	public void StartApp ()
     {
+		/*int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; 
+		if(currentSceneIndex<SceneManager.sceneCount-1)
+		{
+			SceneManager.LoadScene(currentSceneIndex + 1);
+		}*/
         SceneManager.LoadScene("Selfie Capture"); //Load next Scene in the Build Index
     }
 }
