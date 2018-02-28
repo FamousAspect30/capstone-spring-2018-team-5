ef3d36f3a5e2b5c1697fb9eb0100e5e6f5505ac1
jmwooton <justin.wooton@gmail.com>
2018-02-28 11:46:30 -0500

    Added toast message (Android popup) functionality and fixed the save path

--- Unity Project/Legendary 3D Selfie App/Assets/Scripts/SaveScreenMenu.cs
+++ Unity Project/Legendary 3D Selfie App/Assets/Scripts/SaveScreenMenu.cs
@@ -1,14 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class SaveScreenMenu : MonoBehaviour {
 
+    string savePath;
+    public Texture2D picture;
+    private Sprite sprite;
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+        savePath = "/Internal Storage/DCIM/Camera";
+        //GameObject obj = GameObject.Find("Canvas");
+        //picture = obj.GetComponent<CapturePic>().snap;
+       // sprite = Sprite.Create(picture, new Rect(0f, 0f, picture.width, picture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        //obj.GetComponent<SpriteRen>
+    }
 
     public void SavePhoto()
     {
-        return;
+        File.WriteAllBytes(savePath + System.DateTime.Now.ToString() + ".jpg", picture.EncodeToJPG());
     }
 
     public void LoadPreviousScene()
